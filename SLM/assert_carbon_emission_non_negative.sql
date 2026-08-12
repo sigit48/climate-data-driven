807bbdc0a21a0b60c12acb25feb741284eb792e5
Sigit Dwiantoro <sigitantoro048@gmail.com>
2026-08-12 09:04:44 +0700

    Delete SLM directory

--- SLM/assert_carbon_emission_non_negative.sql
+++
@@ -1,7 +0,0 @@
--- Test kustom (singular test): carbon_emission_forecast_mt tidak boleh negatif.
--- Konvensi dbt: test GAGAL kalau query ini mengembalikan >0 baris, LOLOS kalau 0 baris.
--- Tidak butuh package tambahan (dbt_utils/dbt_expectations) -- murni SQL biasa.
-
-SELECT *
-FROM {{ ref('stg_carbon_emissions') }}
-WHERE carbon_emission_forecast_mt < 0
